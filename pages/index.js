288d4fccefdc711f075cdd19fc49d48cfbc92d69
vsemoth <vsemoth@gmail.com>
2021-12-10 23:09:50 +0200

    Altered Index Content

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Lwandile's future blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Got started by editing <code>pages/index.js</code>
         </p>
       </main>
 
